3ecada838a81931091dd0e5a6ef1c298b0857e05
Daniyalsid26 <daniyalsid26@gmail.com>
2025-06-13 11:27:07 +0100

    Add Python Standard Code

---
+++ test code/02_absolute_value/standard_absolute_value.py
@@ -0,0 +1,5 @@
+def absolute_value(n): 
+    if n < 0: 
+        return -n 
+    else: 
+        return n
---
+++ test code/03_sum_array/standard_sum_array.py
@@ -0,0 +1,5 @@
+def sum_array(arr):
+    total = 0
+    for element in arr:
+        total += element
+    return total
