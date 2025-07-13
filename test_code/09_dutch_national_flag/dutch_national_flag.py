15d90eb4e11243cc0ac0cedf1c2c3771ee0d1070
Daniyalsid26 <daniyalsid26@gmail.com>
2025-07-13 23:13:53 +0100

    Update test_code

---
+++ test_code/09_dutch_national_flag/dutch_national_flag.py
@@ -0,0 +1,20 @@
+from typing import List, Optional
+
+def dutch_national_flag(arr: Optional[List[int]]) -> None:
+    if not arr:
+        return
+    
+    low: int = 0
+    mid: int = 0
+    high: int = len(arr) - 1
+    
+    while mid <= high:
+        if arr[mid] == 0:
+            arr[low], arr[mid] = arr[mid], arr[low]
+            low += 1
+            mid += 1
+        elif arr[mid] == 1:
+            mid += 1
+        else:
+            arr[mid], arr[high] = arr[high], arr[mid]
+            high -= 1
